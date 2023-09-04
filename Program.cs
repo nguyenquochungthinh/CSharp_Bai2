eb2fb41c3ca014eab3acf43647808f95d140b20c
nguyenquochungthinh <thinhminhdung239@gmail.com>
2023-09-04 14:04:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten: ");
+            String ten=Console.ReadLine();  
+            Console.WriteLine("HELLO {0}!", ten);
+            Console.ReadKey();
+        }
+    }
+}
